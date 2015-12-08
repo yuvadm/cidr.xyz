a358d615529015f0161754f66aa0fd150f915eaf
Yuval Adam <yuval@y3xz.com>
2015-12-09 00:33:49 +0200

    IPAddress component with changing state

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ import './style.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Note extends React.Component {
+class IPAddress extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      octets: [10, 88, 135, 24],
+      cidr: 36,
+    }
+  }
+
+  handleChange = (event) => {
+    var octets = this.state.octets
+    octets[+event.target.attributes['data-octet'].value] = +event.target.value
+    this.setState({
+      octets: octets
+    })
+  }
+
   render() {
-    return <div>Hai!</div>
+    return <form>
+      <input type="text" data-octet="0" onChange={this.handleChange} value={this.state.octets[0]}/>.
+      <input type="text" data-octet="1" onChange={this.handleChange} value={this.state.octets[1]}/>.
+      <input type="text" data-octet="2" onChange={this.handleChange} value={this.state.octets[2]}/>.
+      <input type="text" data-octet="3" onChange={this.handleChange} value={this.state.octets[3]}/>/
+      <input type="text" onChange={this.handleChange} value={this.state.cidr}/>
+      <h2></h2>
+    </form>
   }
 }
 
-main();
-
-function main() {
-  const app = document.createElement('div')
-  document.body.appendChild(app)
-  ReactDOM.render(<Note />, app)
-}
+const app = document.createElement('div')
+document.body.appendChild(app)
+ReactDOM.render(<IPAddress />, app)
