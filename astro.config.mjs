6e8655a416e75f6a0f8e01a7dd800d9a43d1daf9
Yuval Adam <_@yuv.al>
2025-06-17 13:37:41 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cidr.xyz',
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   devToolbar: {
     enabled: false
   }
